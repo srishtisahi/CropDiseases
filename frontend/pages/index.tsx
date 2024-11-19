55498ec3f03f0ebd2167765b17e756af20007cb7
Srishti Sahi <srishtisahi24@gmail.com>
2024-11-19 14:42:44 +0530

    server link added

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -86,7 +86,7 @@ export default function CropDiseaseDetector() {
           }
 
           // Send matrix to API
-          const response = await fetch('http://localhost:5000/predict', {
+          const response = await fetch('https://cropdiseases.onrender.com/predict', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
